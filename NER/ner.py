b84e39d3512df10982cfd87b9a70f2228eed8ee6
subu131 <subramanian131@gmail.com>
2020-09-29 09:11:54 +0530

    last commit

--- NER/ner.py
+++ NER/ner.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import spacy
 from spacy import displacy
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en')
 HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem">{}</div>"""
 
 # Fetch Text From Url
@@ -21,45 +21,17 @@ def analyze_text(text):
 
 
 
-def main():
+def write():
 	"""Summaryzer Streamlit App"""
 
 	st.title("Entity Checker")
-
-	activities = ["NER Checker","NER For URL"]
-	choice = st.sidebar.selectbox("Select Activity",activities)
-
-	
-	if choice == 'NER Checker':
-		st.subheader("Named Entity Recog with Spacy")
-		raw_text = st.text_area("Enter Text Here","Type Here")
-		if st.button("Analyze"):
-			docx = analyze_text(raw_text)
-			html = displacy.render(docx,style="ent")
-			html = html.replace("\n\n","\n")
-			st.write(HTML_WRAPPER.format(html),unsafe_allow_html=True)
-
-
-	if choice == 'NER For URL':
-		st.subheader("Analysis on Text From URL")
-		raw_url = st.text_input("Enter URL Here","Type here")
-		text_preview_length = st.slider("Length to Preview",50,100)
-		if st.button("Analyze"):
-			if raw_url != "Type here":
-				result = get_text(raw_url)
-				len_of_full_text = len(result)
-				len_of_short_text = round(len(result)/text_preview_length)
-				st.success("Length of Full Text::{}".format(len_of_full_text))
-				st.success("Length of Short Text::{}".format(len_of_short_text))
-				st.info(result[:len_of_short_text])
-				summarized_docx = sumy_summarizer(result)
-				docx = analyze_text(summarized_docx)
-				html = displacy.render(docx,style="ent")
-				html = html.replace("\n\n","\n")
-				st.write(HTML_WRAPPER.format(html),unsafe_allow_html=True)
-				
+	st.subheader("Named Entity Recog with Spacy")
+	raw_text = st.text_area("Enter Text Here","Type Here")
+	if st.button("Analyze"):
+		docx = analyze_text(raw_text)
+		html = displacy.render(docx,style="ent")
+		html = html.replace("\n\n","\n")
+		st.write(HTML_WRAPPER.format(html),unsafe_allow_html=True)
 		
 
 
-if __name__ == '__main__':
-	main()
